8ce9dc0a98c7a52278c126a0b70e06d39250761d
Joseph Okafor <joptimalresource@live.com>
2022-10-09 04:46:14 +0100

    Added the servant leader section

--- components/pages/landing-page/index.js
+++ components/pages/landing-page/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./hero";
+import ServantLeader from "./servant-leader";
 import Welcome from "./welcome";
 function Volunteers({}) {
   return (
     <>
       <Hero />
       <Welcome />
+      <ServantLeader />
     </>
   );
 }
---
+++ components/pages/landing-page/servant-leader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ServantLeader = () => {
+  return (
+    <div className="flex justify-between w-[100%] min-h-[850px] bg-[#F6FCFF] px-[5%] py-[100px] mt-[150px]">
+      <div className="uppercase">Call to Serve</div>
+      <div className="">ServantLeader</div>
+    </div>
+  );
+};
+
+export default ServantLeader;
