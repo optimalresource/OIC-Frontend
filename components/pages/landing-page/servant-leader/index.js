e3ae4c4858a401faf58995c93cc7b8798dd263d9
Joseph Okafor <joptimalresource@live.com>
2022-10-09 12:10:56 +0100

    Continued working on the servant leader section

--- components/pages/landing-page/servant-leader/index.js
+++ components/pages/landing-page/servant-leader/index.js
@@ -2,9 +2,32 @@ import React from "react";
 
 const ServantLeader = () => {
   return (
-    <div className="flex justify-between w-[100%] min-h-[850px] bg-[#F6FCFF] px-[5%] py-[100px] mt-[150px]">
-      <div className="uppercase">Call to Serve</div>
-      <div className="">ServantLeader</div>
+    <div className="flex justify-between bmd:flex-col gap-10 w-[100%] min-h-[850px] bg-[#F6FCFF] px-[5%] py-[100px] mt-[150px]">
+      <div className="flex flex-col flex-1 gap-8">
+        <div className="w-[100%] flex gap-5 items-center">
+          <hr className="w-[100px] max320:w-[50px] border-[2px] border-[#010101]" />
+          <div className="uppercase">Call to Serve</div>
+        </div>
+
+        <h2 className="text-3xl font-semibold">
+          Volunteer to be a{" "}
+          <span className="text-[#1f4477]">Servant Leader</span>
+        </h2>
+
+        <p className="">
+          If you are willing to serve throughout the entire conference, we
+          invite you to submit your application to be a servant leader! You will
+          receive an update on your application status after it has been
+          processed. We appreciate your desire to serve this generation!
+        </p>
+
+        <div className="mt-[50px]">
+          <button className={`buttonPrimary text-sm`}>
+            Become a Servant Leader
+          </button>
+        </div>
+      </div>
+      <div className="flex-1">ServantLeader</div>
     </div>
   );
 };
