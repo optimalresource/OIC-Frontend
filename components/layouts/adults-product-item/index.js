6b8db6640a3167ce47aeaaf7a083214fd8a1f82e
Joseph Okafor <joptimalresource@live.com>
2022-10-23 05:51:56 +0100

    Added the store page

--- components/layouts/adults-product-item/index.js
+++ components/layouts/adults-product-item/index.js
@@ -1,25 +1,18 @@
-import React, {Fragment} from 'react';
-import Image from 'next/image'
-import {Link} from 'react-router-dom'
-import styles from './AdultsProductItem.module.css'
-const AdultsProductItem = ({name, price, imageUrl, paymentLink }) => {
-    console.log('the image', imageUrl)
-    return (
-
-
-
-
-            <div className={styles.container}>
-                <Image src={imageUrl} className={styles.imagecontainer} alt="hi" />
-                <font className={styles.productname}>{name}</font>
-                <div className={styles.paybutton}><a href={paymentLink}><button>&#8358;{price} </button></a></div>
-            </div>
-
-            )
-}
-
-
-
+import React, { Fragment } from "react";
+import Image from "next/image";
+import styles from "./AdultsProductItem.module.css";
+const AdultsProductItem = ({ name, price, imageUrl, paymentLink }) => {
+  return (
+    <div className={styles.container}>
+      <Image src={imageUrl} className={styles.imagecontainer} alt="hi" />
+      <font className={styles.productname}>{name}</font>
+      <div className={styles.paybutton}>
+        <a href={paymentLink}>
+          <button>&#8358;{price} </button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default AdultsProductItem;
-
--- components/layouts/kids-product-item/index.js
+++ components/layouts/kids-product-item/index.js
@@ -1,20 +1,15 @@
-import React, {Fragment} from 'react';
-import Image from 'next/image'
-import styles from './KidsProductItem.module.css'
-import {kidsPaylink} from '../../../data/products'
+import React, { Fragment } from "react";
+import Image from "next/image";
+import styles from "./KidsProductItem.module.css";
+// import {kidsPaylink} from '../../../data/products'
 const KidsProductItem = ({ name, price, imageUrl, paymentLink }) => {
-    
-    return (
-            
-            <div className={styles.container}>
-                
-                <a><Image src={imageUrl}  alt="hi" /></a>
-            </div>
-            )
-}
-
-
-
+  return (
+    <div className={styles.container}>
+      <a>
+        <Image src={imageUrl} alt="hi" />
+      </a>
+    </div>
+  );
+};
 
 export default KidsProductItem;
-
