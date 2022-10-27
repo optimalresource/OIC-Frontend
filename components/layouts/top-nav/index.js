c39b53c643e0e9e1495e6c7110d336c500b3a206
Joseph Okafor <joptimalresource@live.com>
2022-10-28 00:12:30 +0100

    Changed the top section

--- components/layouts/top-nav/index.js
+++ components/layouts/top-nav/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const TopNav = () => {
   return (
-    <div className="fixed flex  py-[10px] top-0 h-[50px] justify-between items-center text-[#fff] z-[200]  px-[5%] w-[100%] max640:hidden min1728:px-[95px] bg-[#000]">
+    <div className="fixed flex  py-[10px] top-0 h-[50px] justify-between items-center text-[#fff] z-[200]  px-[5%] w-[100%] max640:hidden min1728:px-[95px] bg-[#0b508f]">
       <div className="flex items-center gap-5">
         {/* <div className="flex gap-2 items-center">
           <Icon icon="akar-icons:phone" />
