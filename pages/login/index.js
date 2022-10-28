0b3420c4c4b7597907ec7bfb9e749d7698e469e6
Joseph Okafor <joptimalresource@live.com>
2022-10-28 11:49:08 +0100

    Added meta tags

--- pages/login/index.js
+++ pages/login/index.js
@@ -9,8 +9,23 @@ export default function Login() {
         <title>Oasis International Conference - Login</title>
         <meta
           name="description"
-          content="The Oasis International Conference..."
+          content="RCCG The Oasis International Conference usually holds once in a year. It's a gathering of Christians who are hungry for the word and presence of God. Over the years, countless lives have been transformed."
         />
+        <meta
+          name="og:title"
+          content="RCCG The Oasis International Conference"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="rccgoic.com" />
+        <meta
+          property="og:site_name"
+          content="RCCG The Oasis International Conference"
+        />
+        <meta
+          property="og:description"
+          content="RCCG The Oasis International Conference usually holds once in a year. It's a gathering of Christians who are hungry for the word and presence of God. Over the years, countless lives have been transformed."
+        />
+        <meta property="og:image" content="/fav.png" />
         <link rel="icon" href="/fav.png" />
       </Head>
       <div className="textlg font-bold h-[500px] mt-[200px] relative">
