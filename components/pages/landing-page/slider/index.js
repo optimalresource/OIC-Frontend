bd950bc12bc781e9548a3600e89f70c0c92ece9d
Odejimi Damilola <damilolaodejimi@gmail.com>
2022-11-07 13:53:50 +0100

    Slider style update

--- components/pages/landing-page/slider/index.js
+++ components/pages/landing-page/slider/index.js
@@ -4,7 +4,14 @@ import Carousel from "react-bootstrap/Carousel";
 
 function Slider() {
   const banners = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    { eventDay: 'Monday, 21st', time: '6:00pm' },
+    // { eventDay: 'Tuesday, 22nd', time: '6:00pm' },
+    // { eventDay: 'Wednesday, 23rd', time: '6:00pm' },
+    // { eventDay: 'Thursday, 24th', time: '6:00pm' },
+    // { eventDay: 'Friday, 25th', time: '9:00pm' },
+    // { eventDay: 'Saturday, 26th', time: '3:00pm' },
+    // { eventDay: 'Sunday, 27th', time: '12:30pm' },
+    // '8', '9'
   ];
   return (
     <div className="flex justify-center items-center w-[90%] bmd:w-[100%] mt-[130px] bmd:mt-[100px] max640:mt-[60px] relative rounded-[0px]">
@@ -21,29 +28,29 @@ function Slider() {
         <Carousel>
           {banners.map((value, index) => {
             return (
-              <Carousel.Item key={index}>
+              <Carousel.Item key={(index+1)}>
                 <img
-                  src={"/assets/images/slider/" + value + ".png"}
+                  src={"/assets/images/slider/" + (index+1) + ".png"}
                   className="d-block w-100 rounded-[0px]"
                   alt="First slide"
                 />
                 <div
                   style={{ position: "absolute" }}
                   className="md:px-10 py-5 text-[#fff] top-0 left-0 h-[100%] w-[100%] flex flex-col items-center justify-center max400:px-2">
-                  <div className="px-6 md:px-7 lg:px-12 max400:pr-1 max400:pl-1 max320:px-0 text-center">
-                    <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold max400:text-3xl leading-[30px] md:leading-[80px] lg:leading-[80px] cinzel-font">
+                  <div className="max400:px-7 px-8 lg:px-12 text-center">
+                    <h1 className="text-2xl sm:text-5xl lg:text-7xl font-bold leading-[30px] md:leading-[50px] lg:leading-[80px] cinzel-font">
                       Now is the acceptable time
                     </h1>
                   </div>
-                  <div className="flex flex-col items-center bg-[#000] bg-opacity-60 p-3 py-1 mb-[80px] md:mt-10 bmd:mt-5 md:min-w-[500px] bmd:min-w-[300px] max320:min-w-[200px]">
+                  <div className="flex-col items-center bg-[#000] d-none d-sm-flex bg-opacity-60 p-3 py-1 max575:mb-auto mb-[30px] lg:mb-[80px] md:mt-10 bmd:mt-5 md:min-w-[500px] bmd:min-w-[300px] max320:min-w-[200px]">
                     <h3 className="text-base md:text-lg lg:text-xl amd:font-semibold py-1 max400:py-1 max400:text-xs max400:font-normal">
-                      DAY 1
+                      DAY {(index + 1)}
                     </h3>
                     <h2 className="font-semibold text-center text-base md:text-xl lg:text-2xl">
-                      Monday, 21st November
+                       {value.eventDay} November
                     </h2>
                     <p className="text-sm md:text-base lg:text-lg amd:font-semibold max400:font-normal">
-                      Time: 6:00pm
+                      Time: {value.time}
                     </p>
                   </div>
                 </div>
